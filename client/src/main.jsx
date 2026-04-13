cf04b4224735e8fcf8a1aa7603730aaa7b8d6031
Mycah Accad <56968035+codewithMycah@users.noreply.github.com>
2026-04-13 08:03:34 -0500

    Added basic styles and integrate tailwindcss to MUI

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { StyledEngineProvider } from '@mui/material/styles';
+import GlobalStyles from '@mui/material/GlobalStyles';
 import App from './App'
 import './styles/theme.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <StyledEngineProvider enableCssLayer>
+        <GlobalStyles styles="@layer theme, base, mui, components, utilities;" />
+        <App />
+      </StyledEngineProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
