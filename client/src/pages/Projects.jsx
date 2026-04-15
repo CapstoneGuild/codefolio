d23f5de86589dc5caba7764b6774f1742d52b988
Mercy Akinyemi <akinyemimercy293@gmail.com>
2026-04-14 22:46:06 -0500

    Add Projects page component to display project cards

---
+++ client/src/pages/Projects.jsx
@@ -0,0 +1,23 @@
+import ProjectCard from "../components/ui/ProjectCard"
+
+const Projects = () => {
+  const projects = 10
+  return (
+   <div className="h-screen flex flex-col bg-gray-50 rounded-lg">
+      <div className="p-4 border-b">
+        <h1 className="text-xl font-semibold">Projects</h1>
+      </div>
+
+      <div className="flex-1 overflow-y-auto p-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 justify-items-stretch">
+          {[...Array(projects)].map((_, index) => (
+            <ProjectCard key={index} />
+          ))}
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Projects
