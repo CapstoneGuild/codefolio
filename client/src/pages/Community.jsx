173e8d25e203ae28f7b7a5f469e7ad7e5110ae1b
Mercy Akinyemi <akinyemimercy293@gmail.com>
2026-04-14 22:46:28 -0500

    Add Community page component to display a list of PostCard components

---
+++ client/src/pages/Community.jsx
@@ -0,0 +1,22 @@
+import PostCard from "../components/ui/PostCard"
+const Community = () => {
+    const posts = 10;
+    return (
+        <div className="h-screen flex flex-col bg-gray-50 rounded-lg">
+        <div className="p-4 border-b">
+            <h1 className="text-xl font-semibold">Community</h1>
+        </div>
+
+        <div className="flex-1 overflow-y-auto p-4">
+            <div className="flex flex-col gap-4 justify-items-stretch">
+                {[...Array(posts)].map((_, index) => (
+                    <PostCard key={index} />
+                ))}
+            </div>
+        </div>  
+        </div>
+
+    )
+}
+
+export default Community
