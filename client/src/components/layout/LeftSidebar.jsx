605ecc0fa4c44f7bf02d945f74bcf07f7b6957a5
Mercy Akinyemi <126781467+Fis-ayo@users.noreply.github.com>
2026-04-28 23:47:00 -0500

    Remove userId dependency from LeftSidebar

--- client/src/components/layout/LeftSidebar.jsx
+++ client/src/components/layout/LeftSidebar.jsx
@@ -7,16 +7,12 @@ import Person2Icon from '@mui/icons-material/Person2';
 import BookmarkIcon from '@mui/icons-material/Bookmark';
 import WifiTetheringIcon from '@mui/icons-material/WifiTethering';
 
-const LeftSidebar = ({ userId }) => {
+const LeftSidebar = () => {
   const menu = [
     { text: 'Projects', icon: <ArticleIcon />, to: '/projects' },
     { text: 'Community', icon: <GroupsIcon />, to: '/community' },
     { text: 'Network', icon: <WifiTetheringIcon />, to: '/network' },
-    {
-      text: 'Bookmarks',
-      icon: <BookmarkIcon />,
-      to: userId ? `/profile/user/${userId}/bookmarks` : '/bookmarks',
-    },
+    { text: 'Bookmarks', icon: <BookmarkIcon />, to: '/bookmarks' },
   ]
 
   return (
